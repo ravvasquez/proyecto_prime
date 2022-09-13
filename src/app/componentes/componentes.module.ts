5f56e6191aa9035b5d330944689419e155aeed0a
ravvasquez <ramiTaller2022@gmail.com>
2022-09-12 21:43:15 -0300

    Co-authored-by: Jcacero <Jcacero@users.noreply.github.com>

--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {CarouselModule} from 'primeng/carousel';
+import {CardModule} from 'primeng/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ButtonModule} from 'primeng/button';
+import { CardsComponent } from './cards/cards.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    CardModule
+  ],
+  exports:[
+    CarouselModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    CardModule,
+    CardsComponent
   ]
+  
 })
 export class ComponentesModule { }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  items: MenuItem[]=[];
 
-  constructor() { }
+    ngOnInit() {
+        this.items = [
+            { 
+                routerLink:'inicio',
+                label:'inicio',
+                icon:'pi pi-fw pi-home',
+            },
+            {
+                routerLink:'admin',
+                label:'admin',
+                icon:'pi pi-fw pi-user',
 
-  ngOnInit(): void {
-  }
+            },
+            {
+                routerLink:'contacto',
+                label:'contacto',
+                icon:'pi pi-fw pi-envelope',
+            },
+        ];
+    }
+
+  constructor() {
+    
+   }
+
+   
 
 }
